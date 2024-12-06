e998bff0dfc7836270741491c77249ee113a33ba
Martin Mueller <martin.ernst.mueller@gmail.com>
2024-12-06 13:23:44 +0100

    Remove not neede imports

--- SWP_FirstSteps/MyFirstCat/Dog.cs
+++ SWP_FirstSteps/MyFirstCat/Dog.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Cache;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace MyFirstCat
+﻿namespace MyFirstCat
 {
     class Dog(string name, string color, DateTime birthDate) : Animal(name, color, birthDate) 
     {
--- SWP_FirstSteps/MyFirstCat/Tiefheim.cs
+++ SWP_FirstSteps/MyFirstCat/Tiefheim.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Cache;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace MyFirstCat
+﻿namespace MyFirstCat
 {
     class Tierheim()
     {
