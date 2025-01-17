0442565e3cfd831f26bd175c4ae629251966e599
Martin Ernst Mueller <martin.ernst.mueller@gmail.com>
2025-01-17 13:17:52 +0100

    Rename Color

--- SWP_FirstSteps/MyFirstCat/Being.cs
+++ SWP_FirstSteps/MyFirstCat/Being.cs
@@ -2,14 +2,14 @@
 {
     class Being
     {
-        public string Color { get; set; }
+        public string ColorOfAnimal { get; set; }
         public string Name { get; set; }
         private DateTime _birthDate { get; set; }
         public int Age { get { return (DateTime.Now.Year - _birthDate.Year); } }
 
         public Being(string name, string color, DateTime birthDate)
         {
-            Color = color;
+            ColorOfAnimal = color;
             _birthDate = birthDate;
             Name = name;
         }
